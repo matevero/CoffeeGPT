ad3dc23c2c6747bdf231553d80b8f9002b4e4c27
matevero <meucafebrasileiro@gmail.com>
2025-04-16 07:26:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,7 @@ def whatsapp_webhook():
     return str(response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
